6c2055c3441a949e5e53fb6d3ffe9d0c0068a900
vukim <vukim@DESKTOP-QK3D6E0>
2021-12-21 13:22:09 +0700

    Create_Table_LopHoc_VDKO102

---
+++ LTQL_1721050102/LTQL_1721050102/Models/LopHoc102.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050102.Models
+{
+    [Table("LopHoc102")]
+    public class LopHoc102
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MaLop { get; set; }
+        [StringLength(50)]
+        public int TenLop { get; set; }
+    }
+}
